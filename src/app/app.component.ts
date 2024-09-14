8374a573d4e7ad6bd3205db1b5945b965fe16e86
Roberto Petit de Meurville <rpetit@riu.com>
2024-09-14 14:50:59 -0300

    Styles to scss.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list'
+import { MatButton, MatMiniFabButton } from '@angular/material/button';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MatToolbarModule, MatIconModule, 
-    MatSidenavModule, MatListModule],
+    MatSidenavModule, MatListModule, MatButton],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'bogas-web';
